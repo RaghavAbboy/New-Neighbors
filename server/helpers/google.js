b915523ffcf517d749d54a804760b0b809c670b2
raghavabboy <raghavabboy@gmail.com>
2015-09-09 12:31:20 -0700

    [Save] Starts splitting functions.

---
+++ server/helpers/google.js
@@ -0,0 +1,7 @@
+var keys = require('../config/keys.js');
+
+module.exports = {
+  getStreetAddress : function () {
+
+  }
+}
--- server/server.js
+++ server/server.js
@@ -28,67 +28,200 @@ app.post('/api/getNeighbors', function (req, res) {
 	var eventNumber = 0;
 
 	var checkAndRespond = function (neighborhoodObj) {
-		if(eventNumber === 1) {
+		if(eventNumber === numNeighborhoods) {
 			res.status(200).send(neighborhoodObj);
 		}
 	}
 
-	geoCode(searchInfo.address)
-	.then(function (geoCode) {
-		// console.log('Data from geoCode received. EventNumber:', eventNumber);
-		// console.log('geoCode:', geoCode);
+	// geoCode(searchInfo.address)
+	// .then(function (geoCode) {
+	// 	// console.log('Data from geoCode received. EventNumber:', eventNumber);
+	// 	// console.log('geoCode:', geoCode);
+ //    ++eventNumber;
+	// 	userDestination = geoCode;
+	// 	return findNeighborhoods(geoCode);
+	// },function (errorMessage) {
+ //    ++eventNumber;
+ //    checkAndRespond({});
+ //  })
+
+	// .then(function (neighborhoodObj) {
+	// 	console.log('Neighborhoods fetched.');
+ //    if(Object.keys(neighborhoodObj).length === 0) { checkAndRespond({}); }
+	// 	return getStreetAddresses(neighborhoodObj);
+	// })
+
+	// .then(function (neighborhoodObj) {
+	// 	console.log('Street addresses fetched.');
+	// 	return getDistances(neighborhoodObj);
+	// })
+
+ //  .then(function (neighborhoodObj) {
+ //    console.log('Distances fetched.');
+
+ //    //Fetch estimates and Demography Information only if the address is in the United States
+ //    if(country === 'USA') { return getEstimates(neighborhoodObj, searchInfo); }
+ //    else { checkAndRespond(neighborhoodObj); }
+ //  })
+
+ //  .then(function (neighborhoodObj) {
+ //    console.log('Rental Estimates fetched.');
+ //    return getDemographics(neighborhoodObj)
+ //  })
+
+ //  .then(function (neighborhoodObj) {
+ //    console.log('Zillow Info Fetched.');
+ //    console.log('eventNumber:', eventNumber);
+ //    checkAndRespond(neighborhoodObj);
+ //  });
+
+
+//--------------------------------------------------------------------------------------------
+//Refactored promise chain
+
+geoCode(searchInfo.address)
+  .then(function (geoCode) {
+    // console.log('Data from geoCode received. EventNumber:', eventNumber);
+    // console.log('geoCode:', geoCode);
     ++eventNumber;
-		userDestination = geoCode;
-		return findNeighborhoods(geoCode);
-	},function (errorMessage) {
+    userDestination = geoCode;
+    return findNeighborhoods(geoCode);
+  },function (errorMessage) {
     ++eventNumber;
     checkAndRespond({});
   })
 
-	.then(function (neighborhoodObj) {
-		console.log('Neighborhoods fetched.');
+  .then(function (neighborhoodObj) {
+    // console.log('Neighborhoods fetched:',neighborhoodObj);
     if(Object.keys(neighborhoodObj).length === 0) { checkAndRespond({}); }
-		return getStreetAddresses(neighborhoodObj);
-	})
 
-	.then(function (neighborhoodObj) {
-		console.log('Street addresses fetched.');
-		return getDistances(neighborhoodObj);
-	})
-
-  .then(function (neighborhoodObj) {
-    console.log('Distances fetched.');
-
-    //Fetch estimates and Demography Information only if the address is in the United States
-    if(country === 'USA') { return getEstimates(neighborhoodObj, searchInfo); }
-    else { checkAndRespond(neighborhoodObj); }
-  })
-
-  .then(function (neighborhoodObj) {
-    console.log('Rental Estimates fetched.');
-    return getDemographics(neighborhoodObj)
-  })
-
-  .then(function (neighborhoodObj) {
-    console.log('Zillow Info Fetched.');
-    console.log('eventNumber:', eventNumber);
-    checkAndRespond(neighborhoodObj);
+    return startChains(neighborhoodObj);
+    // return getStreetAddresses(neighborhoodObj);
   });
 
-});	//end of POST request handler
+  // .then(function (neighborhoodObj) {
+  //   console.log('Street addresses fetched.');
+  //   return getDistances(neighborhoodObj);
+  // })
+
+  // .then(function (neighborhoodObj) {
+  //   console.log('Distances fetched.');
+
+  //   //Fetch estimates and Demography Information only if the address is in the United States
+  //   if(country === 'USA') { return getEstimates(neighborhoodObj, searchInfo); }
+  //   else { checkAndRespond(neighborhoodObj); }
+  // })
+
+  // .then(function (neighborhoodObj) {
+  //   console.log('Rental Estimates fetched.');
+  //   return getDemographics(neighborhoodObj)
+  // })
+
+  // .then(function (neighborhoodObj) {
+  //   console.log('Zillow Info Fetched.');
+  //   console.log('eventNumber:', eventNumber);
+  //   checkAndRespond(neighborhoodObj);
+  // });
 
 
-//Refactored promise chain
+
+
+});  //end of POST request handler
+
+
+
+//-----------------------------------------------------------------------------------
+//Start a promise chain for each neighborhood from the neighborhood object
+var startChains = function (neighborhoodObj) {
+  var deferred = Q.defer();
+  var neighborhoodArray = Object.keys(neighborhoodObj);
+
+  numNeighborhoods = neighborhoodArray.length;
+  neighborhoodObject = neighborhoodObj;
+
+  //remove
+  console.log('StartChains called.');
+  console.log('Number of neighborhoods:',numNeighborhoods);
+
+  //Start a promise chain/cluster for each neighborhood
+  for(var neighborhood in neighborhoodObject) {
+    console.log('Neighborhood:',neighborhood);
+
+  }
 
 
 
 
+  deferred.resolve(neighborhoodObj);
+  return deferred.promise;
+}
 
 
+//-----------------------------------------------------------------------------------
+//GET list of neighborhood localities for a pair of coordinates (corresponding to the given street address)
+/*Input: coordinates
+  Output: Object of neighborhoods
+*/
 
+var findNeighborhoods = function (geoCode) {
+	var deferred = Q.defer();
 
+	var gPlacesUrl_location = 'https://maps.googleapis.com/maps/api/place/search/json?location=';			//latitude + ',' + longitude
+	var gPlacesUrl_radius = '&radius=';
+	var gPlacesUrl_types = '&types=';
+	var gPlacesUrl_key = 'l&key=';
 
+	var neighborhoodObj = {};
+	var radius = 1000;
+	var numResponses = 1;
+	var key = keys.googleAPIKey;
+	var types = 'locality|sublocality|neighborhood|administrative_area_level_1|administrative_area_level_2|administrative_area_level_3|administrative_area_level_4|administrative_area_level_5|sublocality_level_4|sublocality_level_3|sublocality_level_2|sublocality_level_1';
 
+  for(radius = 1000; radius<=20000; radius+=500) {
+		var gPlacesUrl = gPlacesUrl_location + geoCode.coordinates.latitude + ',' + geoCode.coordinates.longitude +
+										 gPlacesUrl_radius + radius +
+										 gPlacesUrl_types + types +
+										 gPlacesUrl_key + key;
+
+    //remove
+    // console.log('gPlaces:', gPlacesUrl);
+
+		getRequest(gPlacesUrl)
+		.then(function (responseObj) {
+			var results = responseObj.results;
+
+      //remove
+      // console.log('Neighborhood object:', results);
+
+			_.each(results, function (result) {
+				neighborhoodObj[result.name] = neighborhoodObj[result.name] ||
+				{
+          name: result.name,
+					latitude: result.geometry.location.lat,
+					longitude: result.geometry.location.lng,
+					placeId: result.place_id
+				};
+			});
+
+			//remove
+			// console.log('Neighborhoods fetched:',numResponses);
+
+			if(numResponses === 39) { deferred.resolve(neighborhoodObj); }
+			else { numResponses++; }
+		},
+
+    function (errorMessage) {
+      // console.log('Error/server not responding.');
+      // console.log('errorMessage:', errorMessage);
+
+      if(numResponses === 39) { deferred.resolve(neighborhoodObj); }
+      else { numResponses++; }
+    });
+
+	}//end of for loop
+
+	return deferred.promise;
+}
 
 
 
@@ -153,71 +286,6 @@ var queryZillow = function (neighborhood, city) {
 }
 
 
-//-----------------------------------------------------------------------------------
-//GET list of neighborhood localities for a pair of coordinates (corresponding to the given street address)
-/*Input: coordinates
-  Output: Object of neighborhoods
-*/
-
-var findNeighborhoods = function (geoCode) {
-	var deferred = Q.defer();
-
-	var gPlacesUrl_location = 'https://maps.googleapis.com/maps/api/place/search/json?location=';			//latitude + ',' + longitude
-	var gPlacesUrl_radius = '&radius=';
-	var gPlacesUrl_types = '&types=';
-	var gPlacesUrl_key = 'l&key=';
-
-	var neighborhoodObj = {};
-	var radius = 1000;
-	var numResponses = 1;
-	var key = keys.googleAPIKey;
-	var types = 'locality|sublocality|neighborhood|administrative_area_level_1|administrative_area_level_2|administrative_area_level_3|administrative_area_level_4|administrative_area_level_5|sublocality_level_4|sublocality_level_3|sublocality_level_2|sublocality_level_1';
-
-  for(radius = 1000; radius<=20000; radius+=500) {
-		var gPlacesUrl = gPlacesUrl_location + geoCode.coordinates.latitude + ',' + geoCode.coordinates.longitude +
-										 gPlacesUrl_radius + radius +
-										 gPlacesUrl_types + types +
-										 gPlacesUrl_key + key;
-
-    //remove
-    // console.log('gPlaces:', gPlacesUrl);
-
-		getRequest(gPlacesUrl)
-		.then(function (responseObj) {
-			var results = responseObj.results;
-
-      //remove
-      // console.log('Neighborhood object:', results);
-
-			_.each(results, function (result) {
-				neighborhoodObj[result.name] = neighborhoodObj[result.name] ||
-				{
-          name: result.name,
-					latitude: result.geometry.location.lat,
-					longitude: result.geometry.location.lng,
-					placeId: result.place_id
-				};
-			});
-
-			//remove
-			console.log('Neighborhoods fetched:',numResponses);
-
-			if(numResponses === 39) { deferred.resolve(neighborhoodObj); }
-			else { numResponses++; }
-		},
-
-    function (errorMessage) {
-      // console.log('Error/server not responding.');
-      // console.log('errorMessage:', errorMessage);
-
-      if(numResponses === 39) { deferred.resolve(neighborhoodObj); }
-      else { numResponses++; }
-    });
-
-	}//end of for loop
-
-	return deferred.promise;
-}
 
 
 //-----------------------------------------------------------------------------------
@@ -456,12 +524,14 @@ var getStreetAddresses = function (neighborhoodObj) {
         }, wait);
       }
 
+    }, function (errorMessage) {
+      numEvents++;
+      if(numEvents === numNeighborhoods) { deferred.resolve(neighborhoodObj); }
+      else { numEvents++; }
     });
   }
 
   makeRequest(neighborhoodList[0]);
-
-	//deferred.resolve('done');
 	return deferred.promise;
 }
 
